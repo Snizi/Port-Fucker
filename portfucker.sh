f969400cef54d196c71b1b01997474efb6a306b9
Luan Simões <timpsembs.7@gmail.com>
2020-10-02 17:02:29 -0400

    Finish scan with specific ports

--- portfucker.sh
+++ portfucker.sh
@@ -1,54 +1,78 @@
 #!/bin/bash
+
+# ==========================================================================
+# CONSTANTS
+# ==========================================================================
+
+# Constant for the colors
 PINK='\033[1;35m'
 CYAN='\033[1;36m'
 
-
+# Constant that creates a temporary file
 tmpfile=$(mktemp)
 
 
-#check if the file ports.txt exists
-#echo add ports 55 22 >> ports.txt
 
 
-			#menu
-printf "${PINK}=%.0s" {1..100} #print the pink line
+# ==========================================================================
+# Menu
+# ==========================================================================
+print_menu() {
+	printf "${PINK}=%.0s" {1..80} #print the pink line
 
-printf "\n\n${CYAN}\t\t\t\t\tPort Fucker\n\n"
-printf "\t\t\t\t(1)Scan for specific ports\n"
-printf "\t\t\t\t(2)Scan for a range of ports\n"
-printf "\t\t\t\t(3)Scan for specific ports on a IP range\n"
-printf "\t\t\t\t(4)Scan for a range of ports on a IP range\n\n"
+	printf "\n\n${CYAN}\t\t\t\t\tPort Fucker\n\n"
+	printf "\t\t\t(1)Scan for specific ports\n"
+	printf "\t\t\t(2)Scan for a range of ports\n"
+	printf "\t\t\t(3)Scan for specific ports on a IP range\n"
+	printf "\t\t\t(4)Scan for a range of ports on a IP range\n\n"
 
-printf "${PINK}=%.0s" {1..100} #print the pink line
-			#menu
-printf "\n${CYAN}Choose an option: "
+	printf "${PINK}=%.0s" {1..80} #print the pink line
+
+	printf "\n${CYAN}Choose an option: "
+}
+
+
+print_menu #call the function
+
 read menu #read the option picked by the user
 
 case $menu in
 1)
+	printf "Ip address: "
+	read ip
 	printf "Load ports from ports.txt? [Y\N]:"
 	read menu
 	if [ $menu == "Y" ] || [ $menu == "y" ]
 	then
-		printf "Ip address: "
-		read ip
-
 		printf "Scanning on $ip\n"
-
-
 		for port in $(cat ports.txt)
-                do
-                       hping3 -S $ip -c 1 --destport $port 2>/dev/null | grep "flags=SA" | cut -d "=" -f 6 | cut -d " " -f 1 >> "$tmpfile";
+        do
+            openports=$(hping3 -S $ip -c 1 --destport $port 2>/dev/null | grep "flags=SA" | cut -d "=" -f 6 | cut -d " " -f 1);
+			if [ "$openports" != "" ]
+			then
+				printf "${PINK}Port: $openports is open\n"
+			fi
 		done
-
-		#print out the open ports saved in the open_ports.txt file
-		for openport in $(cat "$tmpfile")
+	
+	else
+							#Specific typed ports
+		printf "Type the ports that you want to scan (80, 443...1337): \n"
+		read -ra portsArray 
+		printf "Scanning on $ip\n"
+		for port in "${portsArray[@]}"
 		do
-			printf "${PINK}Port: $openport is open\n"
+			openports=$(hping3 -S $ip -c 1 --destport $port 2>/dev/null | grep "flags=SA" | cut -d "=" -f 6 | cut -d " " -f 1);
+			if [ "$openports" != "" ]
+			then
+				printf "${PINK}Port: $openports is open\n"
+			fi
 		done
-		rm "$tmpfile"
-
 	fi
 ;;
 
 esac #end of the switch
+<<<<<<< HEAD
+ 
+
+=======
+>>>>>>> c895d4cbc20ef8f1bb420c39017f72104398d438
